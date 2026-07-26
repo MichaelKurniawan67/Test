a64ea3d8aa9af8a963818443156bbd108fb89354
AlfredPros <michaelkurniawan67@gmail.com>
2026-07-26 13:23:44 +0700

    Initial Commit

---
+++ T_1.py
@@ -0,0 +1,67 @@
+print("Hello")
+print("World")
+print("Hello World")
+
+print("Hello Cantik")
+
+a = 10
+print(a)
+
+a = 10
+x = 5
+panjang = 1000
+
+# Pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+# Pemanggilan kedua
+print("Nilai a = ", a)
+
+a = 7
+print("Nilai a = ", a)
+
+b = a
+print("Nilai b = ", b)
+
+# type data : Angka Satuan (Integer)
+data_integer = 11
+print("data = ", data_integer)
+print("- bertipe : ", type(data_integer))
+
+# type data : angka dengan koma (float)
+data_float = 1.5
+print("data = ", data_float)
+print("- bertipe : ", type(data_float))
+
+# type data : kumpulan karater (string)
+data_string = "ucup"
+print("data = ", data_string)
+print("- bertipe : ", type(data_string))
+
+# type data : biner true/false (boolean)
+data_bool = True
+print("data = ", data_bool)
+print("- bertipe : ", type(data_bool))
+
+# type data khusus
+
+# bilangan kompleks
+data_complex = complex(5,6)
+print("data = ", data_complex)
+print("- bertipe : ", type(data_complex))
+
+# type data dari bahasa C
+
+from ctypes import c_double
+
+data_c_double = c_double(10.5)
+print("data = ", data_c_double)
+print("- bertipe : ", type(data_c_double))
+
+# lanjut video 6
+
+
+
+
